ee3a397b39696826938716e585f968027946a65e
Siwon-Kim <siwonkim1108@gmail.com>
2024-02-13 18:45:23 +0900

    spring bean JPA

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,21 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    // 스프링 데이터 JPA가 SpringDataJpaMemberRepository 를 스프링 빈으로 자동 등록해준다.
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,13 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 // business logic을 다루는 service class
-@Service
 @Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
