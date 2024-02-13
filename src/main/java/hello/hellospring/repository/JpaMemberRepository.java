b9fee46f9a347f6f287e5a6680b08c7eb57da779
Siwon-Kim <siwonkim1108@gmail.com>
2024-02-13 18:29:39 +0900

    jpa repository

--- src/main/java/hello/hellospring/repository/JpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -1,4 +1,46 @@
 package hello.hellospring.repository;
 
-public class JpaMemberRepository {
+import hello.hellospring.domain.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository {
+
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) { // DI
+        this.em = em;
+    }
+
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id); // pk 기반
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+
+        return result.stream().findAny();
+
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
 }
